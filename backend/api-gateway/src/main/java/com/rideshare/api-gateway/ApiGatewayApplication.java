6b75fe248176d2e33bbf7f31a3152e924f83e279
Manojkushwah-dev <manojkushwah91115@gmail.com>
2025-12-24 09:10:17 +0530

    some changes

--- backend/api-gateway/src/main/java/com/rideshare/api-gateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/rideshare/api-gateway/ApiGatewayApplication.java
@@ -3,7 +3,7 @@ package com.rideshare.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.rideshare")
+@SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
